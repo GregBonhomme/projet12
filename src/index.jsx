5e6f5cfec90956377f3fcea57a7ede667eab79cc
GregBonhomme <gregoire.bonhomme@gmail.com>
2024-04-03 01:57:28 +0200

    mise en place de l'environement de travail et du mockup API

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path='/' element={ <Dashboard /> } />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
